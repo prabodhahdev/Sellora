a56aee42d5cc32f2d83be2a274b5b50ae9b43588
prabodhahdev <prabodaharshani95@gmail.com>
2026-09-22 19:05:02 +0530

    MarketPlace Page

--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -1,8 +1,80 @@
 
+import { ArrowLeftIcon, FilterIcon } from 'lucide-react'
+import { useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import ListingCard from '../components/ListingCard'
+import FilterSidebar from '../components/FilterSidebar'
+
 const Marketplace = () => {
+
+  const [searchParams] = useSearchParams()
+  const search = searchParams.get("search")
+  const navigate = useNavigate()
+  const [showFilterPhone, setFilterPhone] = useState(false)
+  const [filters, setFilters] = useState({
+    platform: null,
+    maxPrice: 100000,
+    minFollowers: 0,
+    niche: null,
+    verified: false,
+    monetized: false,
+  })
+
+
+
+  const { listings } = useSelector(state => state.listing)
+
+  const filteredListings = listings.filter((listing) => {
+
+    if (filters.maxPrice) {
+      if (listing.price > filters.maxPrice) return false
+    }
+
+    if (filters.platform && filters.platform.length > 0) {
+      if (!filters.platform.includes(listing.platform)) return false
+    }
+
+    if (filters.minFollowers) {
+      if (listing.followers_count < filters.minFollowers) return false
+    }
+
+    if (filters.niche && filters.niche.length > 0) {
+      if (!filters.niche.includes(listing.niche)) return false
+    }
+    if (filters.verified && listing.verified !== filters.verified) return false
+    if (filters.monetized && listing.monetized !== filters.monetized) return false
+
+    if (search) {
+      const trimed = search.trim();
+      if (
+        !listing.title.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.username.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.description.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.platform.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.niche.toLowerCase().includes(trimed.toLowerCase())
+      )
+        return false
+    }
+    return true
+  })
+
+
   return (
-    <div>
-      market place [age]
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+      <div className="flex items-center justify-between text-slate-800 ">
+        <button onClick={() => { navigate('/'); scrollTo(0, 0) }} className='flex items-center gap-2 py-5'><ArrowLeftIcon className='size-4' /> Back to Home</button>
+        <button onClick={() => setFilterPhone(true)} className='flex sm:hidden'><FilterIcon className='size-4' /> Filters</button>
+      </div>
+
+      <div className='relative flex items-start justify-between gap-8 pb-8'>
+        <FilterSidebar setFilterPhone={setFilterPhone} filters={filters} showFilterPhone={showFilterPhone} setFilters={setFilters} />
+        <div className='flex-1 grid xl:grid-cols-2 gap-4'>
+          {filteredListings.sort((a, b) => a.featured ? -1 : b.featured ? 1 : 0).map((listing, index) => (
+            <ListingCard listing={listing} key={index} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
